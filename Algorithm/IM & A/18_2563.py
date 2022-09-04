e887f801609ed384885728ccec6300bd2da42759
jupiter6676 <jupiter6676@naver.com>
2022-09-04 19:33:37 +0900

    220904 Algorithm

--- Algorithm/IM & A/18_2563.py
+++ Algorithm/IM & A/18_2563.py
@@ -1,5 +1,5 @@
 # 도화지
-graph = [[0] * 101 for _ in range(101)] 
+graph = [[0] * 100 for _ in range(100)] 
 
 N = int(input())
 for _ in range(N):
@@ -10,8 +10,8 @@ for _ in range(N):
             graph[i][j] = 1
 
 cnt = 0
-for i in range(101):
-    for j in range(101):
+for i in range(100):
+    for j in range(100):
         if graph[i][j]:
             cnt += 1
 
