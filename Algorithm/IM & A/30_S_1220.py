b9fc764abe20cacf4eb2e1bd3075e4ffc5ca5279
jupiter6676 <jupiter6676@naver.com>
2022-09-23 16:43:15 +0900

    220923 Algorithm

---
+++ Algorithm/IM & A/30_S_1220.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+for t in range(1, 11):
+    K = int(input())
+    graph = [list(map(int, input().split())) for _ in range(K)]
+
+    # N극이 위에 있으니까
+    # 빨간 자성체가 있으면, 아래로 내려간다. 파란 자성체를 만나면 교착상태 +1
+    # 빨간 자성체를 만나거나 빈공간이 계속되면, 그대로 떨어짐
+
+    N = 1   # N극(빨간) 자성체
+    S = 2   # S극(파란) 자성체
+
+    total = 0
+
+    # 열부터 탐색
+    for j in range(K):
+        tmp_cnt = 0
+
+        for i in range(K):
+            if graph[i][j] == N and tmp_cnt == 0:
+                tmp_cnt = 1
+            
+            if tmp_cnt == 1 and graph[i][j] == S:
+                total += tmp_cnt
+                tmp_cnt = 0
+
+    print(f'#{t} {total}')
