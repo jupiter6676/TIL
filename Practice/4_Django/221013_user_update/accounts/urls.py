f33c794776863c2b61fd47c82573e8061f4480c3
jupiter6676 <jupiter6676@naver.com>
2022-10-13 16:13:06 +0900

    221013 Django Practice (Auth)
    Signup & Login

--- Practice/4_Django/221013_user_update/accounts/urls.py
+++ Practice/4_Django/221013_user_update/accounts/urls.py
@@ -5,8 +5,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.index, name='index'),    # 회원 목록 조회
-    # path('signup/', views.signup, name='signup'),    # 회원 가입
-    # path('login/', views.login, name='login'),    # 로그인
+    path('signup/', views.signup, name='signup'),    # 회원 가입
+    path('login/', views.login, name='login'),    # 로그인
     # path('logout/', views.logout, name='logout'),    # 로그아웃
     # path('<int:pk>', views.detail, name='detail'),    # 회원 정보 조회
     # path('<int:pk>/update/', views.update, name='update'),    # 회원 정보 수정
--- Practice/4_Django/221013_user_update/accounts/views.py
+++ Practice/4_Django/221013_user_update/accounts/views.py
@@ -1,6 +1,50 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm # 회원가입, 정보 수정
+from django.contrib.auth.forms import AuthenticationForm    # 로그인
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 # 회원 목록 조회 페이지
 def index(request):
     return render(request, 'accounts/index.html')
+
+
+# 회원 가입 페이지
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else:
+        form = CustomUserCreationForm()
+        
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/signup.html', context)
+
+
+# 로그인 페이지
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'index')
+
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/login.html', context)
