a168cb5bbf6b12a63f6c0456a617490df6bf82c0
jupiter6676 <jupiter6676@naver.com>
2023-01-20 17:40:13 +0900

    230120 Algorithm

---
+++ Algorithm/BaekJoon/7570.py
@@ -0,0 +1,12 @@
+N = int(input())
+nums = list(map(int, input().split()))  # 1, 2, 3, ..., N
+dp = [0] * (N + 1)  # 0, 1, 2, 3, ..., N
+
+max_asc = 0
+for i in range(N):
+    n = nums[i]
+
+    dp[n] = dp[n - 1] + 1   # d = 1인 가장 긴 등차수열 찾기
+    max_asc = max(max_asc, dp[n], dp[n - 1] + 1)
+
+print(N - max_asc)
