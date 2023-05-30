618d0a66641c45a0f904dd6de3c3b6d4124a008f
Boyeong <jupiter6676@naver.com>
2023-05-30 20:35:22 +0900

    230530 Algorithm

--- Algorithm/BaekJoon/1719_택배.py
+++ Algorithm/BaekJoon/1719_택배.py
@@ -3,10 +3,9 @@ import heapq
 input = sys.stdin.readline
 
 def pprint(mat):
-    print('================')
     for i in range(1, V + 1):
         for j in range(1, V + 1):
-            print(mat[i][j], end=' ')
+            print(mat[i][j] if mat[i][j] != 0 else '-', end=' ')
         print()
 
 def dijkstra(start):
@@ -14,31 +13,22 @@ def dijkstra(start):
     dists[start] = 0
     
     q = [[0, start]]
-    visited = [0] * (V + 1)
-
     while q:
         w1, v1 = heapq.heappop(q)
 
         for adj, w2 in graph[v1]:
-            if not visited[adj] and dists[adj] >= w1 + w2:
+            if dists[adj] > w1 + w2:
                 dists[adj] = w1 + w2
 
                 if v1 == start:
                     path[start][adj] = adj
                 else:
-                    path[start][adj] = v1
+                    path[start][adj] = path[start][v1]
 
-                # path[v1][adj] = adj
-
-                # path[adj][v1] = v1
-                # path[adj][start] = start
-                visited[adj] = 1
                 heapq.heappush(q, [w1 + w2, adj])
 
-    print(start, dists)
 
-
-
+'''main'''
 V, E = map(int, input().split())
 graph = [[] for _ in range(V + 1)]
 path = [[0] * (V + 1) for _ in range(V + 1)]
@@ -51,8 +41,4 @@ for _ in range(E):
 for i in range(1, V + 1):
     dijkstra(i)
 
-for i in range(V, 0, -1):
-    dijkstra(i)
-
 pprint(path)
-# dijkstar(V)
