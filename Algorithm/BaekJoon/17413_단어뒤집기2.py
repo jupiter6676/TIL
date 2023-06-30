757223560b09d0323e6ab0a04c2aafab6b66186c
Boyeong <jupiter6676@naver.com>
2023-06-30 19:41:01 +0900

    230630 Algorithm

---
+++ Algorithm/BaekJoon/17413_단어뒤집기2.py
@@ -0,0 +1,55 @@
+words = input()
+
+tmp = ''
+is_tag = False
+
+for i in range(len(words)):
+    if words[i] == '<':
+        if len(tmp) > 0:
+            print(tmp[::-1], end='')
+        
+        is_tag = True
+        tmp = ''
+
+
+    if is_tag:        
+        if words[i] == '>':
+            print(tmp + '>', end='')
+            tmp = ''
+            is_tag = False
+
+        elif words[i] == ' ':
+            print(tmp, end=' ')
+            tmp = ''
+
+        else:
+            tmp += words[i]
+
+    else:
+        if words[i] == ' ':
+            print(tmp[::-1], end=' ')
+            tmp = ''
+
+        else:
+            tmp += words[i]
+
+# for i in range(len(words)):
+#     if words[i] == '<':
+#         print(tmp[::-1], end='')
+#         is_tag = True
+#         tmp = ''
+
+#     if is_tag and words[i] == '>':
+#         print(tmp + '>', end='')
+#         tmp = ''
+#         is_tag = False
+
+#     elif words[i] != ' ':
+#         tmp += words[i]
+        
+#     else:
+#         print(tmp[::-1], end='')
+#         tmp = ''
+        
+
+print(tmp[::-1])
